d49e68d3027126860d0b0041c3d3423a78572683
Abdulwahed Mansour <info@sweetlearn.com>
2020-05-15 17:10:47 +0300

    colors

---
+++ crackPass.py
@@ -0,0 +1,20 @@
+from colorama import Fore, Back, Style
+from colorama import init
+init(autoreset=True)
+import hashlib
+
+
+"""
+Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+Style: DIM, NORMAL, BRIGHT, RESET_ALL
+
+"""
+print(Fore.RED + 'some red text')
+print(Fore.YELLOW + 'some red text')
+
+print(Back.CYAN + 'some red text')
+
+
+
+
